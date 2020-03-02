07fe91b5e8223ffd4258213830ec2251726e5a3e
Armand Syahtama <armand.syahtama@gmail.com>
2020-03-02 15:21:36 -0500

    created script to create probe_list

---
+++ src/create_probed_list.py
@@ -0,0 +1,10 @@
+import subprocess
+
+def call_httprobe():
+    print('Setting up probed_list')
+    with open('output.txt', 'a') as output:
+        subprocess.call('type cert_list.txt | docker run -i httprobe > probed_list.txt', shell=True, stdout=output, stderr=output)
+    print('Finished setting up probed_list')
+
+if __name__ == "__main__":
+    call_httprobe()
