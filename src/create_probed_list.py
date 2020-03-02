6d1e2e14a410fa9e0807ea2d9c10b8dc367aea94
Armand Syahtama <armand.syahtama@gmail.com>
2020-03-02 16:10:39 -0500

    set up call for extended cert list

--- src/create_probed_list.py
+++ src/create_probed_list.py
@@ -3,7 +3,8 @@ import subprocess
 def call_httprobe():
     print('Setting up probed_list')
     with open('output.txt', 'a') as output:
-        subprocess.call('type cert_list.txt | docker run -i httprobe > probed_list.txt', shell=True, stdout=output, stderr=output)
+        subprocess.call('type cert_list.txt | docker run -i httprobe >> probed_list.txt', shell=True, stdout=output, stderr=output)
+        subprocess.call('type extended_cert_list.txt | docker run -i httprobe >> probed_list.txt', shell=True, stdout=output, stderr=output)
     print('Finished setting up probed_list')
 
 if __name__ == "__main__":
